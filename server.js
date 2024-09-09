8060630b73e8e316c7f7301608dab79245859b8b
woo0629 <fr.jeong0629@gmail.com>
2024-09-10 01:06:26 +0900

    추가 / listen 포트 설정

--- server.js
+++ server.js
@@ -619,3 +619,8 @@ app.get("/manager/alerts/:id", async (req, res) => {
 app.get("*", function (req, res) {
   res.redirect(frontendUrl);
 });
+
+const port = process.env.SERVER_URL || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
